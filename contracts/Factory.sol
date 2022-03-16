5157c4ecbd211a6d82d64049f19a6772d7ef0a58
daviddrob <david.drobnak26@gmail.com>
2022-03-16 19:18:05 +0100

    fix: get any token price from uniswap

--- contracts/Factory.sol
+++ contracts/Factory.sol
@@ -40,9 +40,10 @@ contract Factory {
         address tokenOut,
         uint256 amountIn
     ) external view returns (uint256[] memory amounts) {
-        address[] memory path = new address[](2);
+        address[] memory path = new address[](3);
         path[0] = address(tokenIn);
-        path[1] = address(tokenOut);
+        path[1] = address(0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2);
+        path[2] = address(tokenOut);
 
         return
             IUniswapV2Router02(0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D)
@@ -54,9 +55,10 @@ contract Factory {
         address tokenOut,
         uint256 amountIn
     ) external view returns (uint256[] memory amounts) {
-        address[] memory path = new address[](2);
+        address[] memory path = new address[](3);
         path[0] = address(tokenIn);
-        path[1] = address(tokenOut);
+        path[1] = address(0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2);
+        path[2] = address(tokenOut);
 
         return
             IUniswapV2Router02(0xd9e1cE17f2641f24aE83637ab66a2cca9C378B9F)
