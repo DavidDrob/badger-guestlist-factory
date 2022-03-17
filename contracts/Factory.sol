2ae988f92abc76f4681f0cc1cf4a1310f1246cd1
daviddrob <david.drobnak26@gmail.com>
2022-03-17 19:32:38 +0100

    feat: get average price

--- contracts/Factory.sol
+++ contracts/Factory.sol
@@ -37,13 +37,55 @@ contract Factory {
         return clone;
     }
 
+    // TODO: use safemath
+    function getAverageTokenPrice(address _token, uint256 _amount)
+        external
+        view
+        returns (uint256)
+    {
+        uint256[] memory quotes = new uint256[](3);
+        uint256 sum;
+
+        uint256 curveQuote = this.getCurveQuote(
+            0xdAC17F958D2ee523a2206206994597C13D831ec7,
+            _token,
+            _amount
+        );
+        uint256 uniQuote = this.getUniQuote(
+            0xdAC17F958D2ee523a2206206994597C13D831ec7,
+            _token,
+            _amount
+        );
+        uint256 sushiQuote = this.getSushiQuote(
+            0xdAC17F958D2ee523a2206206994597C13D831ec7,
+            _token,
+            _amount
+        );
+
+        if (curveQuote > 0) {
+            quotes[0] = curveQuote;
+        }
+        if (uniQuote > 0) {
+            quotes[1] = uniQuote;
+        }
+        if (sushiQuote > 0) {
+            quotes[2] = sushiQuote;
+        }
+
+        for (uint256 i = 0; i < quotes.length; i++) {
+            sum = sum + quotes[i];
+        }
+
+        return sum / quotes.length;
+    }
+
     // TODO: Add more price oracles
 
     function getUniQuote(
         address tokenIn,
         address tokenOut,
         uint256 amountIn
-    ) external view returns (uint256[] memory amounts) {
+    ) external view returns (uint256 amount) {
         address[] memory path = new address[](3);
         path[0] = address(tokenIn);
         path[1] = address(0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2);
@@ -51,14 +93,14 @@ contract Factory {
 
         return
             IUniswapV2Router02(0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D)
-                .getAmountsOut(amountIn, path);
+                .getAmountsOut(amountIn, path)[2];
     }
 
     function getSushiQuote(
         address tokenIn,
         address tokenOut,
         uint256 amountIn
-    ) external view returns (uint256[] memory amounts) {
+    ) external view returns (uint256 amount) {
         address[] memory path = new address[](3);
         path[0] = address(tokenIn);
         path[1] = address(0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2);
@@ -66,7 +108,7 @@ contract Factory {
 
         return
             IUniswapV2Router02(0xd9e1cE17f2641f24aE83637ab66a2cca9C378B9F)
-                .getAmountsOut(amountIn, path);
+                .getAmountsOut(amountIn, path)[2];
     }
 
     // Works for most tokens
